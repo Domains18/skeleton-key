a7b75c816429b53a8a5f205270914c9a642b95f0
domains18 <gibsonsgibson88@gmail.com>
2026-04-07 11:36:47 +0300

    add devDependencies and configuration files for Biome and Prettier

---
+++ config/dir-structure.js
@@ -0,0 +1,20 @@
+
+
+export function getDirStructure() {
+    return [
+        {
+            github: "github",
+            path: '~/Documents/github',
+            children: [
+                {
+                    employments: "employments",
+                    path: '~/Documents/github/employments',
+                },
+                {
+                    projects: "projects",
+                    path: '~/Documents/github/projects'
+                },
+            ]
+        }
+    ]
+}
--- setup.mjs
+++ setup.mjs
@@ -5,3 +5,21 @@ const os = require('os');
 
 //detect OS
 const platform = os.platform();
+
+
+async function commandExists(command){
+    try {
+        await $`which ${command}`;
+        return true;
+    } catch (error) {
+        console.log(error)
+        return false;
+    }
+}
+
+console.log('intitializing skeleton key...');
+
+if (platform === 'win32') {
+    throw new Error('Windows is not supported');
+}
+
