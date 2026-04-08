b0a329f8b9394a3d3a4aaa5382781003ce43bad3
domains18 <gibsonsgibson88@gmail.com>
2026-04-08 12:48:08 +0300

    feat: refactor ZshInstaller, add Bun type definitions, and initialize project documentation

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+- project to bootstrap new laptops with neccessary software and configurations
+- built with typescript and bun shell
+- uses a declarative approach to define software and configurations
--- installers/zsh-installer.ts
+++ installers/zsh-installer.ts
@@ -3,18 +3,75 @@ import chalk from 'chalk'
 import { $ } from 'bun'
 
 
-
-function installZsh(os: string) {
-    if (commandExists("zsh")) {
-        console.log(chalk.blue("skipping zsh installation"))
-        return;
-    } else {
-        switch (os) {
-            case 'darwin':
-                //check if brew is installed
-                if (!commandExists('brew')) {
-                    console.log('brew not installed, installing it')
-                }
+export class ZshInstaller {
+    constructor(private os: string) {}
+    public async callable() {
+        if (commandExists("zsh")) {
+            console.log(chalk.blue("skipping zsh installation"))
+            return;
+        } else {
+            switch (this.os) {
+                case 'darwin':
+                    //check if brew is installed
+                    if (!commandExists('brew')) {
+                        console.log('brew not installed, installing it')
+                        await this.installBrewOnMacAndAddToPath()
+                    }
+                    await this.installZsh(this.os)
+                    break;
+                case 'linux':
+                    await this.installZsh(this.os)
+                    break;
+                default:
+                    console.log(chalk.red("unsupported operating system"))
+                    break;
+            }
         }
     }
+
+
+
+    private async installBrewOnMacAndAddToPath() {
+        try {
+            await $`/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"`
+        } catch (error) {
+            console.log(chalk.red("failed to install brew"))
+            console.log(error)
+        }
+
+        //add brew to path
+        try {
+            await $`eval "$(/opt/homebrew/bin/brew shellenv)"`
+        } catch (error) {
+            console.log(chalk.red("failed to add brew to path"))
+            console.log(error)
+        }
+    }
+
+
+
+    private async installZsh(os: string) {
+        switch (os) {
+            case 'darwin':
+                try {
+                    await $`brew install zsh`
+                } catch (error) {
+                    console.log(chalk.red("failed to install zsh"))
+                    console.log(error)
+                }
+                break;
+            case 'linux':
+                try {
+                    await $`sudo apt-get install zsh`
+                } catch (error) {
+                    console.log(chalk.red("failed to install zsh"))
+                    console.log(error)
+                }
+                break;
+            default:
+                console.log(chalk.red("unsupported operating system"))
+                break;
+        }
+    }
+
 }
